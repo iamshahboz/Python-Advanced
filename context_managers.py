d54e9c5a92a04e17f60adff9de9199b3e27c7120
Shahboz <sharafzodashahboz@gmail.com>
2023-12-12 16:46:44 +0500

    context manager [with] added

---
+++ context_managers.py
@@ -0,0 +1,7 @@
+'''
+The with statement in Python is quite useful tool for properly managing external resources
+in your programs. It allows you to take advantage of existing context managers 
+to automatically handle the setup and teardown phrases whenever you're dealing
+with external resources or with operations that require those phrases.
+
+'''
