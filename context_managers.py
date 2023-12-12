daa555c126441de609f354ea6b16fbf1ddb7d906
Shahboz <sharafzodashahboz@gmail.com>
2023-12-12 17:41:01 +0500

    with statement examples added

--- context_managers.py
+++ context_managers.py
@@ -5,3 +5,35 @@ to automatically handle the setup and teardown phrases whenever you're dealing
 with external resources or with operations that require those phrases.
 
 '''
+
+# try finally approach
+
+# file = open('hello.txt', 'a')
+
+# try:
+#     file.write('\nTHis is new sentance')
+
+# finally:
+#     file.close()
+
+
+# the with statement approach
+
+with open('hello.txt', mode='w') as file:
+    file.write('Adding text using with statement')
+
+
+'''
+Opening files using with statement is generally recommended because it 
+ensures that open file descriptors are automatically closed after the flow 
+of execution leaves the with block code
+'''
+
+
+# using async with statement 
+
+import aiohttp
+import asyncio
+
+
+
