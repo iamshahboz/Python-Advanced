a04d0737179a5512dd4ad5223290e95bbb2fdbbb
Shahboz <sharafzodashahboz@gmail.com>
2023-12-13 16:11:45 +0500

    renamed to log.py

---
+++ log.py
@@ -0,0 +1,8 @@
+import logging
+# we have 5 different log levels
+
+logging.debug('This is a debug message')
+logging.info('This is info message')
+logging.warning('This is warning message')
+logging.error('This is error message')
+logging.critical('This is critical message')
