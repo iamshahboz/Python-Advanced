8639b45035828b72818261ed0683871eb292aac9
Shahboz <sharafzodashahboz@gmail.com>
2023-12-16 11:37:45 +0500

    descriptor changed

--- descriptors.py
+++ descriptors.py
@@ -17,17 +17,41 @@ __delete__(self, obj) -> None
 
 __set_name__(self, owner, name)
 '''
-class Verbose_attribute():
-    def __get__(self,obj, type=None) -> object: 
-        print('Accessing the attribute to get the value')
-        return 42 
-    def __set__(self,obj, value) -> None:
-        print('accessing the attribute to set the value ')
-        raise AttributeError('Can not change the value')
+# class Verbose_attribute():
+#     def __get__(self,obj, type=None) -> object: 
+#         print('Accessing the attribute to get the value')
+#         return 42 
+#     def __set__(self,obj, value) -> None:
+#         print('accessing the attribute to set the value ')
+#         raise AttributeError('Can not change the value')
     
-class Foo():
-    attribute1 = Verbose_attribute()
+# class Foo():
+#     attribute1 = Verbose_attribute()
 
-my_foo_object = Foo()
-x = my_foo_object.attribute1
-print(x)
+# my_foo_object = Foo()
+# x = my_foo_object.attribute1
+# print(x)
+
+# How attributes are accessed with the lookup chain
+
+# class Vehicle():
+#     can_fly = False 
+#     number_of_wheels = 0
+
+# class Car(Vehicle):
+#     number_of_wheels = 4
+
+#     def __init__(self,color):
+#         self.color = color
+
+# my_car = Car('red')
+# print(my_car.__dict__)
+# print(type(my_car).__dict__)
+
+my_tuple = [2,5,7,9,12,99]
+
+print(dir(my_tuple))
+
+
+
+
